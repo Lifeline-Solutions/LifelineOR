be0f9a56dedea63a73eea27a53678c9e49620788
ger619 <draker74@gmail.com>
2023-10-27 13:22:59 +0300

    added sidebar page

--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,5 @@ Rails.application.routes.draw do
   resources :consultations, only: %i[index show new create destroy edit update]
   resources :profile, only: %i[index show new create destroy edit update]
   resources :next, only: %i[index show new create destroy edit update]
+  resources :exist, only: %i[index show new create destroy edit update]
 end
